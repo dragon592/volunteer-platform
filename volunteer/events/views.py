684ef630fb5ed5c50a1442253eefcbf13a477988
Simon <dggmura@gmail.com>
2025-12-02 15:42:33 +0600

    First commit

---
+++ volunteer/events/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+from django.shortcuts import render
+from .models import Event
+
+def event_list(request):
+    events = Event.objects.all()
+    return render(request, 'events/event_list.html', {'events': events})
+
