9953c9cd1a7419507a6befece32fed2c78c02a79
Mura <you@example.com>
2026-02-20 07:39:41 +0000

    Fix ALLOWED_HOSTS for Render

--- volunteer/settings.py
+++ volunteer/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,12 @@ SECRET_KEY = 'django-insecure-lry=vrw&w1$68vfcwkf&*#cbwt=q)rrk2en(4lfs^4%02p0x5t
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['volunteer-platform-main.onrender.com', '127.0.0.1', 'localhost']
+ # если уже есть вверху — второй раз не добавляй
+
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+render_host = os.getenv('RENDER_EXTERNAL_HOSTNAME')
+if render_host:
+    ALLOWED_HOSTS.append(render_host)
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
